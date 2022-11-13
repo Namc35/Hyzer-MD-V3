a835fc56b9fadd258a1b84294c6605cbcc39c959
Namc35 <111208255+Namc35@users.noreply.github.com>
2022-11-13 12:51:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['+6285696693695'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
+global.nameowner = 'nusa-mc'
+global.numberowner = '6285696693695'
 global.instagram = 'https://instagram.com/hyzer_stfu'
 global.github = 'https://github.com/Hyzerr'
 global.dana = '087892711054'
@@ -13,8 +13,8 @@ global.pulsa = '085212023766'
 global.gopay = '087892711054'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Yuno Gasai Bot'
-global.gc = 'bit.ly/AnyaBot-Group'
+global.namebot = 'nusa. Botzz'
+global.gc = 'bit.ly/'
 global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
